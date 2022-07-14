14477e62430941b2913aaeac685ba169a95722d8
Panit24 <lgwamhmmams@gmail.com>
2022-07-14 23:06:14 +0700

    change port

--- app.js
+++ app.js
@@ -1,50 +1,50 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const authRouter = require("./routes/authRoute");
-const userRouter = require("./routes/userRoute");
-const productAdminRouter = require("./routes/productAdminRoute");
-const productRouter = require("./routes/productRoute");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const authRouter = require('./routes/authRoute');
+const userRouter = require('./routes/userRoute');
+const productAdminRouter = require('./routes/productAdminRoute');
+const productRouter = require('./routes/productRoute');
 
-const adminAuthRouter = require("./routes/adminAuthRoute");
-const cartProductRouter = require("./routes/cartProductRoute");
-const saleOrderRouter = require("./routes/saleOrderRoute");
-const adminRouter = require("./routes/adminRoute");
+const adminAuthRouter = require('./routes/adminAuthRoute');
+const cartProductRouter = require('./routes/cartProductRoute');
+const saleOrderRouter = require('./routes/saleOrderRoute');
+const adminRouter = require('./routes/adminRoute');
 
-const notFoundMiddleware = require("./middleware/notFound");
-const errorMiddleware = require("./middleware/error");
-const authenticate = require("./middleware/authenticate");
-const authenticateAdmin = require("./middleware/authenticateAdmin");
+const notFoundMiddleware = require('./middleware/notFound');
+const errorMiddleware = require('./middleware/error');
+const authenticate = require('./middleware/authenticate');
+const authenticateAdmin = require('./middleware/authenticateAdmin');
 
 const app = express();
 
 app.use(cors());
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/authAdmin", adminAuthRouter);
-app.use("/auth", authRouter);
-app.use("/users", authenticate, userRouter);
-app.use("/admins", authenticateAdmin, adminRouter);
-app.use("/admin_products", authenticateAdmin, productAdminRouter);
-app.use("/products", productRouter);
-app.use("/cart_products", authenticate, cartProductRouter);
-app.use("/sale_orders", authenticate, saleOrderRouter);
+app.use('/authAdmin', adminAuthRouter);
+app.use('/auth', authRouter);
+app.use('/users', authenticate, userRouter);
+app.use('/admins', authenticateAdmin, adminRouter);
+app.use('/admin_products', authenticateAdmin, productAdminRouter);
+app.use('/products', productRouter);
+app.use('/cart_products', authenticate, cartProductRouter);
+app.use('/sale_orders', authenticate, saleOrderRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
 
-const { sequelize } = require("./models");
+const { sequelize } = require('./models');
 const {
   CartProduct,
   User,
   Product,
   SaleOrder,
   SaleOrderProduct,
-} = require("./models");
+} = require('./models');
 //sequelize.sync({ force: true });
 // User.sync({ alter: true });
 // Product.sync({ alter: true });
@@ -52,4 +52,4 @@ const {
 // SaleOrderProduct.sync({ alter: true });
 // sequelize.sync({ alter: true });
 const port = process.env.PORT || 8000;
-app.listen(port, () => console.log("server running on port:" + port));
+app.listen(port, () => console.log('server running on port:' + port));
